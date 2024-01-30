dc9661424868135ff8da22c6beea427b74a7d7b8
Ron Tseytlin <ronts390@gmail.com>
2024-01-30 21:50:57 +0200

    remove escape from validator

--- app/validators/post.ts
+++ app/validators/post.ts
@@ -2,6 +2,6 @@ import vine from '@vinejs/vine'
 
 export const createPostValidator = vine.compile(
   vine.object({
-    body: vine.string().trim().escape().maxLength(300).minLength(1),
+    body: vine.string().trim().maxLength(300).minLength(1),
   })
 )
